4f534328039bde8e86f13c95603c8f1c814d6bc6
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2023-11-23 08:58:19 +0100

    Variablen_Oerationen_fertig

---
+++ Javascript/01 JS Variablen_Operationen/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+let a = 5;
+let b = 12;
+let c = 17;
+
+let result = a + b + c;
+
+console.log(result);
+
+a = a * 16;
+b = a *13;
+c = c + 17;
+
+console.log(a,b,c);
