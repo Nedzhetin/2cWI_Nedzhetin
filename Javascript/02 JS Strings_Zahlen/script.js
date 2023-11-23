3152cf38e087ea2b2d7eadb46fc98f58fc9840b7
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2023-11-23 09:33:18 +0100

    EndOfLesson

---
+++ Javascript/02 JS Strings_Zahlen/script.js
@@ -0,0 +1,10 @@
+let vorname = "Nedzhetin";
+let lastname = "Nedzhetin";
+
+console.log(vorname +" " + lastname);
+
+let a = 17.4;
+let b = 3.9;
+let result = a/b;
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
---
+++ Javascript/03_Aufgabe_zu_Variablen/script.js
@@ -0,0 +1,34 @@
+ // Erstelle eine Variable banana mit dem Wert "Banane"
+// Erstelle eine Variable apple mit dem Wert "Apple"
+
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+// Preis von 8 Äpfeln
+// Preis von 17 Bananen
+// Preis von 1 Tonne Äpfel
+// Preis von 1 Tonne Bananen
+
+
+
+let banana = "Banane";
+let apple = "Apple";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+
+
+let oneApplePrice = (0.34 * bananaPricePerKilo);
+let oneBananaPrice = (0.22 * bananaPricePerKilo);
+
+
+console.log("Preis von 8 Äpfeln = "  + oneApplePrice.toFixed(2) * 8);
+console.log("Preis von 17 Bananen = "  + oneBananaPrice.toFixed(2) * 17);
+console.log("Preis von 1 Tonne Äpfel = "  + oneApplePrice.toFixed(2) * 1000);
+console.log("Preis von 1 Tonne Bananen = "  + oneBananaPrice.toFixed(2) * 1000);
+
