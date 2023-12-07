b4b13d8797ab8f4ff8dd321d4d3eac44ebcffe9d
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2023-12-07 08:34:33 +0100

    08_Switch_Statement_finished

---
+++ Javascript/08 Switch_Statement/script.js
@@ -0,0 +1,25 @@
+let randomNumber = Math.round(Math.random() * (10-5) +5);
+
+console.log(randomNumber);
+
+switch(randomNumber){
+    case 10:
+        console.log("Ten")
+        break;
+    case 9:
+         console.log("Nine")
+         break;
+    case 8:
+         console.log("Eight")
+         break;
+    case 7:
+         console.log("Seven")
+         break;
+    case 6:
+         console.log("Six")
+         break;
+    case 5:
+        console.log("Five")
+         break;
+        
+}
