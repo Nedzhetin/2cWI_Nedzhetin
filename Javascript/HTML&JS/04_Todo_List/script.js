b19dca3b98a74792f7136c6be2a0ee2338991a3a
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2024-02-01 09:32:07 +0100

    EndOfLesson

--- Javascript/HTML&JS/04_Todo_List/script.js
+++ Javascript/HTML&JS/04_Todo_List/script.js
@@ -2,18 +2,18 @@ let submitButton = document.getElementById("button");
 let inputText = document.getElementById("innerText");
 let list = document.getElementById("list");
 
-submitButton.addEventListener("click", function() {
-   let paragraph = document.createElement("p");
-   let delButton = document.createElement("button");
-   delButton.innerHTML = "delete";
-   paragraph.innerHTML = innerText.value;
-   paragraph.appendChild(delButton);
-   delButton.addEventListener("click", function() {
-      paragraph.remove();
-   });
-   paragraph.classList.add("item");
-   delButton.classList.add("deleteButton");
-   paragraph.classList.add("paragraphStyle");
-   list.appendChild(paragraph);
-   inputText.value = "";
+submitButton.addEventListener("click", function () {
+  let paragraph = document.createElement("p");
+  let delButton = document.createElement("button");
+  delButton.innerHTML = "delete";
+  paragraph.innerHTML = innerText.value;
+  paragraph.appendChild(delButton);
+  delButton.addEventListener("click", function () {
+    paragraph.remove();
+  });
+  paragraph.classList.add("item");
+  delButton.classList.add("deleteButton");
+  paragraph.classList.add("paragraphStyle");
+  list.appendChild(paragraph);
+  inputText.value = "";
 });
---
+++ Javascript/JS&Ajax/script.js
@@ -0,0 +1,3 @@
+document.getElementById("load").addEventListener("click", function () {
+  alert("jsdlfk");
+});
