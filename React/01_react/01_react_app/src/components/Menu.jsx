6aa0ba4987b627ec4a6884b6fb5e679dfbef48fc
Nejo <nedzhetin.nedzhetin@student.htldornbirn.at>
2024-05-23 09:28:18 +0200

    react_Übung

---
+++ React/01_react/01_react_app/src/components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <div>
+      <ul>
+        <li>aplle</li>
+        <li>banana</li>
+        <li>kiwi</li>
+      </ul>
+    </div>
+  );
+}
---
+++ React/01_react/01_react_app/src/components/list.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function list() {
+  return <div>list</div>;
+}
